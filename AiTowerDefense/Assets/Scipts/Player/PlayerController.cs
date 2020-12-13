86b6792f789d79878b0d043ee48be0b8a875b710
wojciechwojciak <wwojtek1994@gmail.com>
2020-12-13 06:26:51 +0100

    Player Movement & Working Scenes & new tags

---
+++ AiTowerDefense/Assets/Scipts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb2d;
+    float moveHorizontal;
+    float moveVertical;
+    private Vector2 moveDirection;
+
+    void Update()
+    {
+        MovementInputs();
+    }
+
+    void MovementInputs()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector2(moveHorizontal, moveVertical);
+        rb2d.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);
+
+    }
+
+ 
+}

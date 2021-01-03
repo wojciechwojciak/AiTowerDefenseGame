7522ac32c30ca81c551405429aaebd76e1c582d0
Krzysztof Batko <KBatko11@gmail.com>
2021-01-03 17:20:48 +0100

    ai-first-step

--- AiTowerDefense/Assets/Scipts/Enemies/EnemiesAI.cs
+++ AiTowerDefense/Assets/Scipts/Enemies/EnemiesAI.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class EnemiesAI : MonoBehaviour
 {
+    // Public variables
+    public float movementSpeed;
+
+    // Private variables
+    private bool isDead;
+    private bool isWandering;
+    private bool detectedEnemy;
+    private bool isStuned;
+    private int stunTime;
+    private int HealthPoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isDead = false;
+        HealthPoints = 100;
     }
 
     // Update is called once per frame

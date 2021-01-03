8e245d7ee1b9065694c154a8c3c3aa5afbc6e378
Krzysztof Batko <KBatko11@gmail.com>
2021-01-03 21:58:10 +0100

    random wander

--- AiTowerDefense/Assets/Scipts/Enemies/EnemiesAI.cs
+++ AiTowerDefense/Assets/Scipts/Enemies/EnemiesAI.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class EnemiesAI : MonoBehaviour
 {
-    // Public variables
-    public float movementSpeed;
+    Rigidbody2D rb;
+    Transform tr;
+
+    // Private global variables
+    private float MAX_WANDER_SPEED = 2;
+    private float MAX_CHASING_SPEED = 5;
+    private float WANDER_RING_RADIUS = 10;
 
     // Private variables
+    private Vector2 TargetLocation;
+
     private bool isDead;
-    private bool isWandering;
+    private bool isChasing;
     private bool detectedEnemy;
     private bool isStuned;
     private int stunTime;
@@ -18,13 +25,43 @@ public class EnemiesAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         isDead = false;
+        isChasing = false;
+        detectedEnemy = false;
+        isStuned = false;
+        stunTime = 0;
         HealthPoints = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isChasing == false)
+        {
+            // Wander
+            if (rb.position == TargetLocation)
+            {
+                Wander();
+            }
+            else
+            {
+                rb.position = Vector2.MoveTowards(rb.position, TargetLocation, MAX_WANDER_SPEED * Time.deltaTime);
+            }    
+        }
+
+    }
+
+    public Vector2 Vector2FromAngle(float angle)
+    {
+        angle *= Mathf.Deg2Rad;
+        return new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    }
+
+    void Wander()
+    {
+        Vector2 SinAndCosFromAngle = Vector2FromAngle(Random.Range(0, 360));
+        TargetLocation = SinAndCosFromAngle * WANDER_RING_RADIUS + rb.position;
+        Debug.Log("\nTarget Location: "+ TargetLocation + "   Sinus And Cosinus From Angle: " + SinAndCosFromAngle);
     }
 }
